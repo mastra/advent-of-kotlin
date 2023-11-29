f2da8e726b26004f55c41ed4f018632b9a7fd31e
daniel-mastracchio-simplisafe <daniel.mastracchio@simplisafe.com>
2023-11-29 13:08:09 -0300

    Day 1 solution and test files

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,70 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var elf = 1
+        var maxCal = 0
+        var elfCal = 0
+
+        for (line in input) {
+            if (line!="") {
+                val cal = line.toInt()
+                elfCal += cal
+            } else {
+                if (elfCal>maxCal)  {
+                    maxCal = elfCal
+                }
+                elf++
+                elfCal =0
+            }
+        }
+        if (elfCal>0) {
+            maxCal = maxOf(elfCal, maxCal)
+        }
+        println("elf: $elf")
+        println("maxCal: $maxCal")
+        return maxCal
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calList = mutableListOf<Int>()
+        var elf = 1
+        var maxCal = 0
+        var elfCal = 0
+
+        for (line in input) {
+            if (line!="") {
+                val cal = line.toInt()
+                elfCal += cal
+            } else {
+                if (elfCal>maxCal)  {
+                    maxCal = elfCal
+                }
+                calList.add(elfCal)
+
+                elf++
+                elfCal =0
+            }
+        }
+        if (elfCal>0) {
+            calList.add(elfCal)
+        }
+        val newList = calList.sortedDescending()
+        println("elf: $elf")
+        println("maxCal: $maxCal")
+        val sum = newList[0]+newList[1]+newList[2]
+        println("Sum: $sum")
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = Path("src/Day01_test.txt").readLines()
+    check(part1(testInput) == 10)
+    part1(testInput).println()
+    part2(testInput).println()
 
-    val input = readInput("Day01")
+    val input = Path("src/Day01.txt").readLines()
     part1(input).println()
     part2(input).println()
 }
